556dc1bae83f62fba7f4bcb241df4ee146cdb6ef
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-30 13:46:54 +0300

    Smth changed for REDUX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import '@babel/polyfill';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import App from './components/App.jsx';
+import { fetchTasks } from './actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/* eslint-disable no-underscore-dangle */
+const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
+const devtoolMiddleware = ext && ext();
+/* eslint-enable */
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    devtoolMiddleware,
+  ),
+);
+
+store.dispatch(fetchTasks());
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+/*ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
